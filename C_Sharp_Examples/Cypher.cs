7fbb94ff3ac3392f2fd4ad3886a50a4314dcd79f
David Bourke <davidrbourke@gmail.com>
2017-06-01 12:27:23 +0100

    Cypher class added to demo AES class

---
+++ C_Sharp_Examples/Cypher.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace C_Sharp_Examples
+{
+    public class Cypher
+    {
+        public byte[] EncryptAes(string subjectToEncrypt, byte[] key, byte[] IV)
+        {
+            byte[] encryptedSubject;
+            using (Aes aes = Aes.Create())
+            {
+                // aes.Key & aes.IV set but create another object to get new ones
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(key, IV);
+
+                using (MemoryStream encyptionMemoryStream = new MemoryStream())
+                {
+                    using (CryptoStream encryptionCryptoStream = new CryptoStream(encyptionMemoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter writer = new StreamWriter(encryptionCryptoStream))
+                        {
+                            writer.Write(subjectToEncrypt);
+                        }
+                        encryptedSubject = encyptionMemoryStream.ToArray();
+                    }
+                }
+
+            }
+
+            return encryptedSubject;
+        }
+
+        public string DecryptAes(byte[] subjectToDecrypt, byte[] key, byte[] IV)
+        {
+            string decryptedSubject;
+            using (Aes aes = Aes.Create())
+            {
+                // aes.Key & aes.IV set but create another object to get new ones
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(key, IV);
+
+                using (MemoryStream decryptionMemoryStream = new MemoryStream(subjectToDecrypt))
+                {
+                    using (CryptoStream decryptionCryptoStream = new CryptoStream(decryptionMemoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(decryptionCryptoStream))
+                        {
+                            decryptedSubject = reader.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return decryptedSubject;
+        }
+    }
+}
---
+++ C_Sharp_Examples_Tests/CyperTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Security.Cryptography;
+using C_Sharp_Examples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C_Sharp_Examples_Tests
+{
+    [TestClass]
+    public class CypherTests
+    {
+        [TestMethod]
+        public void EncryptAes_StringProvided_ByteArrayReturned()
+        {
+            // ARRANGE
+            Cypher c = new Cypher();
+            Aes aes = Aes.Create();
+
+            // ACT
+            var encryptedByteArray = c.EncryptAes("Hello world!", aes.Key, aes.IV);
+
+            // ASSERT
+            Assert.IsInstanceOfType(encryptedByteArray, typeof(byte[]));
+        }
+
+        [TestMethod]
+        public void EncryptAes_StringProvided_StringEncrypted()
+        {
+            // ARRANGE
+            Cypher c = new Cypher();
+            Aes aes = Aes.Create();
+            var encryptedByteArray = c.EncryptAes("Hello world!", aes.Key, aes.IV);
+
+            // ACT
+            string decryptedSubject = c.DecryptAes(encryptedByteArray, aes.Key, aes.IV);
+
+            // ASSERT
+            Assert.AreEqual("Hello world!", decryptedSubject);
+        }
+
+        [TestMethod]
+        public void EncryptAes_SameKeySameIV_SameByteEncription()
+        {
+            // ARRANGE
+            Cypher c = new Cypher();
+            Aes aes = Aes.Create();
+
+            // ACT
+            var encryptedByteArrayA = c.EncryptAes("Hello world!", aes.Key, aes.IV);
+            var encryptedByteArrayB = c.EncryptAes("Hello world!", aes.Key, aes.IV);
+
+            // ASSERT
+            Assert.AreEqual(encryptedByteArrayA[0], encryptedByteArrayB[0]);
+            Assert.AreEqual(encryptedByteArrayA[encryptedByteArrayA.Length - 1], encryptedByteArrayB[encryptedByteArrayB.Length - 1]);
+        }
+
+        [TestMethod]
+        public void EncryptAes_SameKeyDifferentIV_DifferentByteEncription()
+        {
+            // ARRANGE
+            Cypher c = new Cypher();
+            Aes aes = Aes.Create();
+            Aes aesB = Aes.Create();
+
+            // ACT
+            var encryptedByteArrayA = c.EncryptAes("Hello world!", aes.Key, aes.IV);
+            var encryptedByteArrayB = c.EncryptAes("Hello world!", aes.Key, aesB.IV);
+
+            // ASSERT
+            Assert.AreNotEqual(encryptedByteArrayA[0], encryptedByteArrayB[0]);
+            Assert.AreNotEqual(encryptedByteArrayA[encryptedByteArrayA.Length - 1], encryptedByteArrayB[encryptedByteArrayB.Length - 1]);
+        }
+
+        [TestMethod]
+        public void DecryptAes_SameKeyDifferentIV_SameKeyDecryptsToSameData()
+        {
+            // This test is to demonstrate that the same key can be used with a different Initialization Vector
+
+            // ARRANGE
+            Cypher c = new Cypher();
+            Aes aes = Aes.Create();
+            Aes aesB = Aes.Create();
+
+            // ACT
+            var encryptedByteArrayA = c.EncryptAes("Hello world!", aes.Key, aes.IV);
+            var decryptedSubjectA = c.DecryptAes(encryptedByteArrayA, aes.Key, aes.IV);
+            var encryptedByteArrayB = c.EncryptAes("Hello world!", aes.Key, aesB.IV);
+            var decryptedSubjectB = c.DecryptAes(encryptedByteArrayB, aes.Key, aesB.IV);
+
+            // ASSERT
+            Assert.AreEqual("Hello world!", decryptedSubjectA);
+            Assert.AreEqual("Hello world!", decryptedSubjectB);
+        }
+    }
+}
