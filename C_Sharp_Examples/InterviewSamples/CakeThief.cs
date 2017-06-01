bc0d09c661468e540e4f9653d6d0e6172e0f24ac
David Bourke <davidrbourke@gmail.com>
2017-06-01 16:38:20 +0100

    Interview samples folder added

---
+++ C_Sharp_Examples/InterviewSamples/CakeThief.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Examples.InterviewSamples
+{
+    //https://www.interviewcake.com/question/csharp/cake-thief?utm_source=weekly_email&utm_campaign=weekly_email&utm_medium=email
+    public class CakeThief
+    {
+        public int MaxDuffelBagValue(IEnumerable<CakeType> cakeTypes, int capacity)
+        {
+            // Sort by most valueable ratio
+            var sortedList = cakeTypes.ToList().OrderByDescending(x => x.Ratio);
+            int valueInBag = 0;
+            int weightInBag = 0;
+            foreach (var cakeType in sortedList)
+            {
+                decimal fit = Math.Floor((decimal)(capacity / cakeType.Weight));
+                if (fit >= 1)
+                {
+                    weightInBag += Convert.ToInt32(fit * cakeType.Weight);
+                    valueInBag = valueInBag + Convert.ToInt32(fit * cakeType.Value);
+                    capacity = capacity - weightInBag;
+                }
+                if (fit < 1 && cakeType.Ratio == 1)
+                    break;
+            }
+
+            return valueInBag;
+        }
+    }
+
+    public class CakeType
+    {
+        public int Weight { get; }
+        public long Value { get; }
+
+        public float Ratio
+        {
+            get { return Value / Weight; }
+        }
+
+        public CakeType(int weight, long value)
+        {
+            Weight = weight;
+            Value = value;
+        }
+    }
+}
---
+++ C_Sharp_Examples_Tests/InterviewSamples/CakeThiefUnitTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using C_Sharp_Examples.InterviewSamples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C_Sharp_Examples_Tests.InterviewSamples
+{
+    [TestClass]
+    public class CakeThiefTests
+    {
+        [TestMethod]
+        public void MaxDuffelBagValue_FullBagPossible_FullBagReturned()
+        {
+            // Arrange
+            IList<CakeType> cakeTypes = new List<CakeType>();
+            CakeType typeA = new CakeType(7, 160);
+            CakeType typeB = new CakeType(3, 90);
+            CakeType typeC = new CakeType(2, 15);
+            cakeTypes.Add(typeA);
+            cakeTypes.Add(typeB);
+            cakeTypes.Add(typeC);
+
+            // Act
+            CakeThief c = new CakeThief();
+            int maxValue = c.MaxDuffelBagValue(cakeTypes, 20);
+
+            // Assert
+            Assert.AreEqual(555, maxValue);
+        }
+
+        [TestMethod]
+        public void MaxDuffelBagValue_HigherValueWithLowerWeight_OptimalValueReturned()
+        {
+            // Arrange
+            IList<CakeType> cakeTypes = new List<CakeType>();
+            CakeType typeA = new CakeType(1, 30);
+            CakeType typeB = new CakeType(50, 200);
+            cakeTypes.Add(typeA);
+            cakeTypes.Add(typeB);
+
+            // Act
+            CakeThief c = new CakeThief();
+            int maxValue = c.MaxDuffelBagValue(cakeTypes, 100);
+
+            // Assert
+            Assert.AreEqual(3000, maxValue);
+        }
+
+        [TestMethod]
+        public void MaxDuffelBagValue_OptimalValueUsingOneType_OptimalValueReturned()
+        {
+            // Arrange
+            IList<CakeType> cakeTypes = new List<CakeType>();
+            CakeType typeA = new CakeType(3, 40);
+            CakeType typeB = new CakeType(5, 70);
+            cakeTypes.Add(typeA);
+            cakeTypes.Add(typeB);
+
+            // Act
+            CakeThief c = new CakeThief();
+            int maxValue = c.MaxDuffelBagValue(cakeTypes, 9);
+
+            // Assert
+            Assert.AreEqual(120, maxValue);
+        }
+    }
+}
